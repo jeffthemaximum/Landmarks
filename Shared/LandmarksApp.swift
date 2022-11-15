ad25d68a2ac261f1c4d53b2ecaa3e368246d5789
Jeff Maxim <frey.maxim@gmail.com>
2022-11-15 11:33:03 -0500

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Jeff Maxim on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
